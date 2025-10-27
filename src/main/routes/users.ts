6fafc572ce203a4f240b4f86ed3b221709bda290
Caleb <lima.cardoso.caleb@gmail.com>
2025-10-27 16:44:20 -0300

    refactor: atualizar imports das rotas de usuário

--- src/main/routes/users.ts
+++ src/main/routes/users.ts
@@ -1,14 +1,8 @@
 import { FastifyInstance, FastifyPluginAsync } from "fastify";
 
-import { makeCreateAdminController } from "../../app/controllers/admin/factories/makeCreateAdminController";
 import { makeSignInController } from "../../app/controllers/users/factories/makeSignInController";
 import { makeSignUpController } from "../../app/controllers/users/factories/makeSignUpController";
-import { UserRole } from "../../infra/models/User";
 import { routeAdapter } from "../adapters/routeAdapter";
-import {
-  CreateAdminInput,
-  createAdminSchema,
-} from "../schemas/createAdminSchema";
 import { signInSchema } from "../schemas/signInSchema";
 import { signUpSchema } from "../schemas/signUpSchema";
 
@@ -25,14 +19,5 @@ export const userRoutes: FastifyPluginAsync = async (
     "/signin",
     { schema: signInSchema },
     routeAdapter(makeSignInController(fastify))
-  );
-
-  fastify.post<{ Body: CreateAdminInput }>(
-    "/create-admin",
-    {
-      schema: createAdminSchema,
-      preHandler: [fastify.authenticate, fastify.authorize([UserRole.ADMIN])],
-    },
-    routeAdapter(makeCreateAdminController())
   );
 };
